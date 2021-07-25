8c9d363527fd7aae49675eb85667be258095f367
Utpal Pati <utpalpati8@gmail.com>
2021-07-25 04:11:38 +0000

    refactor: base setup

---
+++ frontend/src/Data.js
@@ -0,0 +1,63 @@
+
+Data=[
+    {
+        id:1,
+        image:"https://www.myntra.com/tshirts/roadster/roadster-men-white-brush-printed-round-neck-t-shirt/1824369/buy",
+        price:314,
+        size:"M",
+        brand:"Roadster",
+        idealFor:"Men",
+
+        
+    },
+    {
+        id:2,
+        image:"https://www.myntra.com/tshirts/moda-rapido/moda-rapido-men-maroon-printed-round-neck-t-shirt/2378362/buy",
+        price:389,
+        size:"S",
+        brand:"Moda Rapido",
+        idealFor:"Men",
+        
+        
+    },
+    {
+        id:3,
+        image:"https://www.myntra.com/kurta-sets/indo-era/indo-era-women-navy-blue-printed-kurta-with-trousers--dupatta/13515828/buy",
+        price:1057,
+        size:"XL",
+        brand:"Indo Era",
+        idealFor:"Women",
+        
+        
+    },   
+     {
+        id:4,
+        image:"https://www.myntra.com/kurta-sets/nayo/nayo-women-blue-floral-printed-kurta-with-trousers--dupatta/13771262/buy",
+        price:1357,
+        size:"L",
+        brand:"Nayo",
+        idealFor:"Women",
+        
+        
+    },
+    {
+        id:5,
+        image:"https://www.myntra.com/kurta-sets/tiber-taber/tiber-taber-boys-white-printed-panchakattu/8445083/buy",
+        price:1200,
+        size:"S",
+        brand:"Tiber Taber",
+        idealFor:"Kids",
+        
+        
+    },
+    {
+        id:6,
+        image:"https://www.myntra.com/lehenga-choli/bitiya-by-bhama/bitiya-by-bhama-off-white--pink-ready-to-wear-lehenga-with-blouse/11139230/buy",
+        price:1000,
+        size:"S",
+        brand:"JBN Creation",
+        idealFor:"Kids",
+        
+        
+    }
+]
